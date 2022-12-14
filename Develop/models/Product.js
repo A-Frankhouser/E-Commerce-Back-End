f3bf0b8ea2aab9df60e7f81ad2979b87c4e87ca0
Alex Frankhouser <freesoul423@gmail.com>
2022-12-14 18:36:11 -0500

    Completed product.js file in models folder

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,48 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // ID column
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+
+    // Product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    // Price Column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    // Stock Column 
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    // Category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
